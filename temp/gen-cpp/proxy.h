72f650e680f68bba63f48c887e5b7936f39670b5
Gav Verma <verma.gav@gmail.com>
2014-03-26 06:52:19 -0400

    Moved files into gen-cpp

---
+++ temp/gen-cpp/proxy.h
@@ -0,0 +1,18 @@
+#ifndef __PROXY_H
+#define __PROXY_H
+
+#include "cache.h"
+#include <curl/curl.h>
+
+class Proxy {
+  private:
+    Cache _cache;
+    
+    CURL* _curl_handle;
+    CURLcode _curl_response;
+  public:
+    void init();
+    std::string fetch(std::string url);
+};
+
+#endif // __PROXY_H
